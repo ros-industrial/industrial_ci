d179d25a465cb63e8fd2b63b58a92d9e64eb04ec
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-05-06 06:07:29 +0900

    Add an author to a script

--- ros_pre-release.sh
+++ ros_pre-release.sh
@@ -29,7 +29,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-## Author: Isaac I. Y. Saito
+## Author: Isaac I. Y. Saito, Mathias Lè´‰dtke
 
 set -e
 set -x
