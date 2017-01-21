01fae3337a28fbd772b354de895ff20d7c71a86f
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-01-21 15:53:42 +0100

    splitted prerelease tests

--- industrial_ci/src/ros_pre-release.sh
+++ industrial_ci/src/ros_pre-release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright (c) 2016, Isaac I. Y. Saito
+# Copyright (c) 2017, Mathias Lüdtke
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -46,26 +47,41 @@ function run_ros_prerelease() {
     ici_time_end  # setup_environment
 
     ici_time_start setup_prerelease_scripts
-    mkdir -p /tmp/prerelease_job; cd /tmp/prerelease_job; 
+    export WORKSPACE=/tmp/prerelease_job
+    mkdir -p $WORKSPACE; cd /tmp/prerelease_job; 
     local reponame=$PRERELEASE_REPONAME
-    if [ ! "$reponame" ]; then
+    local clone_underlay=true
+    if [ -z "$reponame" ]; then
         reponame=$TARGET_REPO_NAME
         mkdir -p catkin_workspace/src
         cp -a $TARGET_REPO_PATH catkin_workspace/src/
+        clone_underlay=false
     fi
     generate_prerelease_script.py https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml $ROS_DISTRO default ubuntu ${os_code_name} amd64 ${reponame} --level $downstream_depth --output-dir ./
     ici_time_end  # setup_prerelease_scripts
 
-    ici_time_start run_prerelease
-    ./prerelease.sh -y;
-    ici_time_end  # run_prerelease
-    
+    if [ "$clone_underlay" = true ]; then 
+        ici_time_start prerelease_clone_underlay.sh
+        ./prerelease_clone_underlay.sh
+        ici_time_end  # prerelease_clone_underlay
+    fi
+
+    ici_time_start prerelease_build_underlay.sh
+    ./prerelease_build_underlay.sh
+    ici_time_end  # prerelease_build_underlay.sh
+
+    if [ "$downstream_depth" != "0" ]; then
+        ici_time_start run_prerelease_clone_overlay.sh
+        ./prerelease_clone_overlay.sh
+        ici_time_end  # run_prerelease_clone_overlay
+        ici_time_start prerelease_build_overlay.sh
+        ./prerelease_build_overlay.sh
+        ici_time_end  # prerelease_build_overlay.sh
+    fi
+
     ici_time_start show_testresult
     source /opt/ros/${catkin_distro}/setup.bash
     catkin_test_results --verbose || error 'ROS Prerelease Test failed'
     echo 'ROS Prerelease Test went successful.'
     ici_time_end  # show_testresult
-    
-    cd $TARGET_REPO_PATH  # cd back to the repository's home directory with travis
-    pwd
 }
