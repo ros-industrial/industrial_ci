fc47aa3c10acc0562984702248f30c5f2253ef52
Isaac I.Y. Saito <130s@users.noreply.github.com>
2018-08-10 07:35:11 -0700

    Revert "[WIP] rerun_ci script"

--- industrial_ci/src/docker.sh
+++ industrial_ci/src/docker.sh
@@ -65,8 +65,6 @@ function ici_require_run_in_docker() {
 #######################################
 function ici_run_cmd_in_docker() {
   local run_opts=($DOCKER_RUN_OPTS)
-  local commit_image=$_COMMIT_IMAGE
-  unset _COMMIT_IMAGE
 
   #forward ssh agent into docker container
  local ssh_docker_opts=()
@@ -110,9 +108,6 @@ function ici_run_cmd_in_docker() {
   local ret=0
   wait %% || ret=$?
   trap - INT
-  if [ -n "$commit_image" ]; then
-    docker commit -m "$_COMMIT_IMAGE_MSG" "$cid" $commit_image > /dev/null
-  fi
   docker rm "$cid" > /dev/null
   return $ret
 }
