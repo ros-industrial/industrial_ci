f865f38f7ed3049048bff25b672b66034eb574e6
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2019-01-02 23:58:14 -0800

    [docker] Use util function https://github.com/ros-industrial/industrial_ci/pull/299/files#r201686749

--- industrial_ci/src/docker.sh
+++ industrial_ci/src/docker.sh
@@ -176,7 +176,7 @@ function ici_prepare_docker_image() {
   elif [ -z ${DOCKER_IMAGE+x} ]; then # image was not provided, use default
      ici_build_default_docker_image
   elif [ -z "$DOCKER_IMAGE" ]; then
-     echo "Empty string passed to DOCKER_IMAGE." \
+     error "Empty string passed to DOCKER_IMAGE." \
      "Specify a valid docker image or unset the environment variable to use the default image." 1>&2
      exit 1
   elif [ "$DOCKER_PULL" != false ]; then
