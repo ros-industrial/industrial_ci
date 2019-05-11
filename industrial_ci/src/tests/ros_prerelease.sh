243605e1067f28fd2ace817d9d81c419e1b09a2a
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-05-11 17:43:43 +0200

    refactored prerelease, added ROS2 support

--- industrial_ci/src/tests/ros_prerelease.sh
+++ industrial_ci/src/tests/ros_prerelease.sh
@@ -20,75 +20,70 @@
 # It is dependent on environment variables that need to be exported in advance
 # (As of version 0.4.4 most of them are defined in env.sh).
 
-function setup_environment() {
+function setup_ros_prerelease() {
+    useradd -m -d "$WORKSPACE/home" ci
+
+    if [ -e /var/run/docker.sock ]; then
+        groupadd -o -g "$(stat -c%g /var/run/docker.sock)" host_docker
+        usermod -a -G host_docker ci
+    fi
+
+    ici_quiet ici_asroot apt-get -qq install -y docker.io python3-pip python3-yaml sudo
+    ici_asroot pip3 install git+https://github.com/ros-infrastructure/ros_buildfarm.git
+}
+
+function prepare_prerelease_workspaces() {
+  local -a ws_upstream ws_target
+  ici_parse_env_array ws_upstream UPSTREAM_WORKSPACE
+  ici_parse_env_array ws_target TARGET_WORKSPACE
+  local workspace=$1
+  local reponame=$2
+  local targetname=$3
+  ici_with_ws "$workspace/ws" ici_prepare_sourcespace "$workspace/ws/src/" "${ws_upstream[@]}" "${ws_target[@]}"
+
+  if ! [ -d "$workspace/ws/src/$reponame" ]; then
+      mv "$workspace/ws/src/$targetname" "$workspace/ws/src/$reponame"
+  fi
+
+  local -a overlay
+  ici_parse_env_array overlay DOWNSTREAM_WORKSPACE
+  ici_with_ws "$workspace/ws_overlay" ici_prepare_sourcespace "$workspace/ws_overlay/src/" "${overlay[@]}"
+  chown -R ci "$workspace"
+}
+
+function run_ros_prerelease() {
+    local ws; ws=$(mktemp -d)
     export WORKSPACE
-    WORKSPACE=$(mktemp -d)
-    echo "WORKSPACE: $WORKSPACE"
+    local -a opts
+    ici_parse_env_array opts DOCKER_RUN_OPTS
+    opts+=(-e TRAVIS -e OS_NAME -e OS_CODE_NAME -e OS_ARCH -e PRERELEASE_DOWNSTREAM_DEPTH -e PRERELEASE_REPONAME -e ROSDISTRO_INDEX_URL
+                 -v "$ws:$ws:rw" -e "WORKSPACE=$ws")
 
     if [ -n "$DOCKER_PORT" ]; then
-        DIND_OPTS=(-e "DOCKER_HOST=$DOCKER_PORT")
-        user_cmd="useradd ci"
+        opts+=(-e "DOCKER_HOST=$DOCKER_PORT")
     elif [ -e /var/run/docker.sock ]; then
-        DIND_OPTS=(-v /var/run/docker.sock:/var/run/docker.sock)
-        user_cmd="groupadd -o -g $(stat -c%g /var/run/docker.sock) host_docker && useradd -G host_docker ci"
-    else
-        ici_error "Could not detect docker settings"
+        opts+=(-v /var/run/docker.sock:/var/run/docker.sock)
     fi
-    ici_quiet docker build -t "industrial-ci/prerelease" - <<EOF
-FROM ros:melodic-ros-core
-RUN apt-get update -qq \
-    && apt-get -qq install --no-install-recommends -y \
-        docker.io \
-        git \
-        python-ros-buildfarm \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-RUN $user_cmd
-USER ci
-ENV WORKSPACE $WORKSPACE
-WORKDIR $WORKSPACE
-ENTRYPOINT ["/opt/ros/melodic/env.sh"]
-EOF
-}
+    DOCKER_RUN_OPTS="${opts[*]}" DOCKER_IMAGE=${DOCKER_IMAGE:-ros:$ROS_DISTRO-ros-core} ici_require_run_in_docker
 
-function run_in_prerelease_docker() {
-    ROS_DISTRO='' ici_run_cmd_in_docker "${DIND_OPTS[@]}" \
-                                      -v "$WORKSPACE:$WORKSPACE:rw" \
-                                      -e TRAVIS \
-                                      "industrial-ci/prerelease" \
-                                      "$@"
+    ici_run "setup_ros_prerelease" setup_ros_prerelease
 
-}
-function run_ros_prerelease() {
     # Environment vars.
     local downstream_depth=${PRERELEASE_DOWNSTREAM_DEPTH:-"0"}
-
-    ici_time_start setup_environment
-    setup_environment
-    ici_time_end  # setup_environment
-
-    ici_time_start setup_prerelease_scripts
-    mkdir -p "$WORKSPACE/ws/src/"
     local reponame=${PRERELEASE_REPONAME:-$TARGET_REPO_NAME}
-    cp -a "$TARGET_REPO_PATH" "$WORKSPACE/ws/src/$reponame"
 
-    # ensure access rights
-    ROS_DISTRO='' ici_run_cmd_in_docker "${DIND_OPTS[@]}" -v "$WORKSPACE:$WORKSPACE:rw"  --user root  "industrial-ci/prerelease" chown -R ci:ci "$WORKSPACE"
-
-
-    if [ "${USE_MOCKUP// }" != "" ]; then
-        if [ ! -d "$TARGET_REPO_PATH/$USE_MOCKUP" ]; then
-            ici_error "mockup directory '$USE_MOCKUP' does not exist"
-        fi
-        cp -a "$TARGET_REPO_PATH/$USE_MOCKUP" "$WORKSPACE/ws/src"
+    if [ -z "$ROSDISTRO_INDEX_URL" ]; then
+      if [ "$ROS_VERSION" -eq 2 ]; then
+          ROSDISTRO_INDEX_URL="https://raw.githubusercontent.com/ros2/ros_buildfarm_config/ros2/index.yaml"
+          ici_quiet ici_install_pkgs_for_command colcon python3-colcon-common-extensions
+      else
+          ROSDISTRO_INDEX_URL="https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml"
+      fi
     fi
 
-    run_in_prerelease_docker generate_prerelease_script.py https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml "$ROS_DISTRO" default "$OS_NAME" "$OS_CODE_NAME" "${OS_ARCH:-amd64}" --level "$downstream_depth" --output-dir . --custom-repo "$reponame::::"
-    ici_time_end  # setup_prerelease_scripts
-
-    ici_time_start prerelease.sh
-    run_in_prerelease_docker env ABORT_ON_TEST_FAILURE=1 ./prerelease.sh -y
-    ici_time_end  # prerelease.sh
+    ici_run "prepare_prerelease_workspaces" prepare_prerelease_workspaces "$WORKSPACE" "$reponame" "$(basename "$TARGET_REPO_PATH")"
+    ici_run 'generate_prerelease_script' sudo -EH -u ci generate_prerelease_script.py "${ROSDISTRO_INDEX_URL}" "$ROS_DISTRO" default "$OS_NAME" "$OS_CODE_NAME" "${OS_ARCH:-amd64}" --level "$downstream_depth" --output-dir "$WORKSPACE" --custom-repo "$reponame::::"
+    ABORT_ON_TEST_FAILURE=1 ici_run "run_prerelease_script" sudo -EH -u ci sh -c "mkdir -p ~/.ccache && . '/opt/ros/$ROS_DISTRO/setup.sh' && cd '$WORKSPACE' && exec ./prerelease.sh -y"
 
     echo 'ROS Prerelease Test went successful.'
 }
