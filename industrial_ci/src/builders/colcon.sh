ae88188eae606d738f2c357cf31c4ff2482ed9fb
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-10-15 21:11:57 +0200

    fix ROS2 ardent

--- industrial_ci/src/builders/colcon.sh
+++ industrial_ci/src/builders/colcon.sh
@@ -19,7 +19,7 @@ _colcon_event_handlers=(desktop_notification- status- terminal_title-)
 
 function builder_setup {
     ici_install_pkgs_for_command colcon python3-colcon-common-extensions
-    if [ "$ROS_DISTRO" = "kinetic" ]; then
+    if [ "$ROS_DISTRO" = "kinetic" ] || [ "$ROS_DISTRO" = "ardent" ]; then
         ici_install_pkgs_for_command pip3 python3-pip
         ici_asroot pip3 install -U setuptools
     fi
