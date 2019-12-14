62929888675ee098d41f43ebedb7970894f61771
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-12-14 11:04:59 +0100

    implement folding for Github Actions

--- .github/action.js
+++ .github/action.js
@@ -3,6 +3,7 @@ var {spawnSync} = require('child_process');
 var env = Object.assign({}, process.env)
 env.TARGET_REPO_PATH = env.GITHUB_WORKSPACE.slice()
 env.TARGET_REPO_NAME = env.GITHUB_REPOSITORY.split('/').pop()
+env._FOLDING_TYPE = 'github_actions'
 var r = spawnSync(__dirname + '/../ci.sh', { stdio: 'inherit', env });
 if (r.error) {
   throw r.error;
