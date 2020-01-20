0cbca745e55e0ee4a9df48b4b94a469b05ad8d1b
fmessmer <felixmessmer@gmail.com>
2020-01-20 17:46:02 +0100

    add example with invalid python3 syntax

---
+++ industrial_ci/mockups/test_pylint/script/sample_talker2.py
@@ -0,0 +1,11 @@
+# Taken from http://wiki.ros.org/rospy/Overview/Publishers%20and%20Subscribers
+import rospy
+from std_msgs.msg import String
+
+pub = rospy.Publisher('spokenwords', String, queue_size=10)
+rospy.init_node('node_name')
+r = rospy.Rate(10)  # 10hz
+while not rospy.is_shutdown():
+    pub.publish("hello world")
+    print "hello"
+    r.sleep()
