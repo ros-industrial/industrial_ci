536464d0652db9fb50a74e974f4833d77b32cb75
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2020-05-06 20:24:52 +0200

    switch ROS2 foxy from nightly build to testing

--- industrial_ci/src/env.sh
+++ industrial_ci/src/env.sh
@@ -131,8 +131,7 @@ function set_ros_variables {
         ros2_defaults "bionic"
         ;;
     "foxy")
-        ros2_defaults "bionic"
-        DOCKER_IMAGE=${DOCKER_IMAGE:-osrf/ros2:nightly}
+        ros2_defaults "focal"
         ;;
     esac
 
