da8288fcedfc23bc2f12d079d14fe46568f0cead
Chen Bainian <brian97cbn@gmail.com>
2020-07-05 12:00:22 +0800

    Fix Shell Check error
    Signed-off-by: Chen Bainian <brian97cbn@gmail.com>

--- industrial_ci/src/util.sh
+++ industrial_ci/src/util.sh
@@ -40,6 +40,7 @@ function ici_source_setup {
   [[ $- =~ u ]] || u_set=0
   set +u
   # shellcheck disable=SC1090
+  # shellcheck disable=SC1091
   source "$1/setup.bash"
   if [ $u_set ]; then
     set -u
