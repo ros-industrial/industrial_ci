1b9409b3d84961f11c5c52e5f69ec283091aa652
Lukas Solanka <lsolanka@gmail.com>
2020-08-26 15:39:51 +0200

    add an option to not run the CI in a docker container

--- industrial_ci/src/tests/source_tests.sh
+++ industrial_ci/src/tests/source_tests.sh
@@ -101,7 +101,9 @@ function run_source_tests {
     # shellcheck disable=SC1090
     source "${ICI_SRC_PATH}/builders/$BUILDER.sh" || ici_error "Builder '$BUILDER' not supported"
 
-    ici_require_run_in_docker # this script must be run in docker
+    if [ -z "$DO_NOT_RUN_IN_DOCKER" ]; then
+        ici_require_run_in_docker # this script must be run in docker
+    fi
     upstream_ws=~/upstream_ws
     target_ws=~/target_ws
     downstream_ws=~/downstream_ws
