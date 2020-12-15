d644aac627ef6357374e18c43f77625fc966c45c
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2020-12-15 17:54:49 +0100

    Install black only if needed

--- industrial_ci/src/tests/black_check.sh
+++ industrial_ci/src/tests/black_check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function run_black_check() {
-  DOCKER_IMAGE="$DOCKER_BASE_IMAGE" ici_require_run_in_docker # this script must be run in docker
+  DOCKER_IMAGE=${DOCKER_IMAGE:-ros:${ROS_DISTRO}-ros-base} ici_require_run_in_docker # this script must be run in docker
 
-  ici_time_start install_black
-  ici_quiet ici_asroot apt-get install -qq -y python3-pip
-  ici_quiet ici_asroot pip3 -q install black
-  ici_time_end
+  if ! command -v "black" > /dev/null; then
+    ici_time_start install_black
+    ici_quiet ici_install_pkgs_for_command pip3 python3-pip python3-setuptools python3-wheel
+    ici_asroot pip3 install black
+    ici_time_end # install_black
+  fi
 
-  ici_time_start run_black_check
-  black --check --diff --verbose --exclude "/\..*/" "$TARGET_WORKSPACE"  # Exclude hidden directories.
-  ici_time_end
+  ici_run run_black_check black --check --color --diff --verbose --exclude "/\..*/" "$TARGET_WORKSPACE"  # Exclude hidden directories.
 }
