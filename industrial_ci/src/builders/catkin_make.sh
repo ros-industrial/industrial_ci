e3b40db21eb6da719cd5d580f83af2ed465dc2dd
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2021-08-01 21:13:31 +0200

    Add support for BUILDER=catkin_make_devel

--- industrial_ci/src/builders/catkin_make.sh
+++ industrial_ci/src/builders/catkin_make.sh
@@ -23,6 +23,12 @@ function _append_job_opts() {
         _append_job_opts_res+=("-j$jobs" "-l$jobs")
     fi
 }
+function _run_catkin_make () {
+    local target=$1; shift
+    local extend=$1; shift
+    local ws=$1; shift
+    ici_cmd ici_exec_in_workspace "$extend" "$ws" catkin_make "$@" --make-args "$target"
+}
 
 function builder_setup {
     ici_install_pkgs_for_command catkin_make "ros-${ROS_DISTRO}-catkin"
@@ -33,7 +39,7 @@ function builder_run_build {
     local ws=$1; shift
     local opts=()
     _append_job_opts opts PARALLEL_BUILDS 0
-    ici_cmd ici_exec_in_workspace "$extend" "$ws" catkin_make --make-args install "${opts[@]}" "$@"
+    _run_catkin_make install "$extend" "$ws" "${opts[@]}" "$@"
 }
 
 function builder_run_tests {
@@ -41,7 +47,7 @@ function builder_run_tests {
     local ws=$1; shift
     local opts=()
     _append_job_opts opts PARALLEL_TESTS 1
-    ici_cmd ici_exec_in_workspace "$extend" "$ws" catkin_make --make-args run_tests "${opts[@]}" "$@"
+    _run_catkin_make run_tests "$extend" "$ws" "${opts[@]}" "$@"
 }
 
 function builder_test_results {
---
+++ industrial_ci/src/builders/catkin_make_devel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (c) 2021, Mathias Lüdtke
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# shellcheck source=industrial_ci/src/builders/catkin_tools.sh
+source "${ICI_SRC_PATH}/builders/catkin_make.sh"
+
+ici_warn "BUILDER=catkin_make_devel should only be used in addition to the other non-devel builders"
+
+function ici_extend_space {
+    echo "$1/devel"
+}
