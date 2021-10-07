b710728a80a9ce2d3f34cd541b1816934a2a0db8
fmessmer <felixmessmer@gmail.com>
2021-10-07 17:01:29 +0200

    use python module structure for run_gha

--- industrial_ci/python/industrial_ci/common.py
+++ industrial_ci/python/industrial_ci/common.py
@@ -82,21 +82,6 @@ def read_yaml(p):
     with open(p) as f:
         return yaml.safe_load(f)
 
-# read global and job-specific envs from
-def read_job(config):
-    job_envs = []
-    global_env = ''
-    if isinstance(config, dict):
-        for l in config['strategy']['matrix']['env']:
-            job_env = ''
-            for key, value in l.items():
-                job_env += key+"='"+str(value) + "' " # adds a space to every element
-            job_env = job_env[:-1] # remove space from last element
-            job_envs.append(job_env)
-        for key, value in config['env'].items():
-            global_env += " "+key+"='"+str(value)+"'"
-    return global_env, job_envs
-
 def read_allow_failures(config):
     try:
         af =  config['matrix']['allow_failures']
@@ -161,57 +146,3 @@ Only DOCKER_PORT, SSH_AUTH_SOCK, and TERM will be kept.
 Additional variable names can be passed at the end.
 
 """ % cmd)
-
-def main(scripts_dir, argv):
-    if '--help' in argv:
-        print_help(argv[0])
-        sys.exit(0)
-
-    args, extra_env = parse_extra_args(argv[1:])
-
-    path, args = find_config_file(args, ['.github/workflows/industrial_ci_action.yml','.github/workflows/industrial_ci_action.yaml'])
-    config = read_yaml(path)
-    global_env, job_envs = read_job(config['jobs']['industrial_ci'])
-    allow_failures = read_allow_failures(config)
-    job_envs =   [ x for x in job_envs if x not in allow_failures ] \
-               + [None] * read_num_include(config) \
-               + [ x for x in job_envs if x in allow_failures ]
-
-    if len(args) == 0:
-        if(len(global_env) > 0):
-            print('Globals: %s' % str(highlight_diff(global_env)))
-        jobs = len(job_envs)
-        digits = len(str(jobs))
-        for i in range(jobs):
-            print('Job %s%s: %s' % ( str(i+1).rjust(digits),
-                                     ' (allow_failures)' if job_envs[i] in allow_failures else '',
-                                     highlight_diff(job_envs[i]) if job_envs[i] is not None else "<unsupported job from 'include' section>"))
-        print("run all with %s -" % sys.argv[0])
-        sys.exit(0)
-
-    ranges = parse_ranges(args, -1)
-
-    run_ci = [os.path.join(scripts_dir, "run_ci"), os.path.dirname(path), filter_env(global_env)]
-    run_ci_diff = [os.path.join(scripts_dir, "run_ci"), os.path.dirname(path), highlight_diff(global_env, 44)]
-
-    bash = ['env', '-i'] +list(map(gen_env, ['DOCKER_PORT', 'HOME', 'PATH', 'SSH_AUTH_SOCK', 'TERM'])) + ['bash','-e']
-
-    selection = set(apply_ranges(ranges, len(job_envs)))
-
-    for i in selection:
-        if job_envs[i] is None:
-            print("\033[1;43mSkipped job %d, because jobs from 'include' section are not supported\033[1;m" %(i+1,))
-            continue
-        cmd = ' '.join(run_ci + [filter_env(job_envs[i])] + list(map(gen_env, extra_env)))
-        cmd_diff = ' '.join(run_ci_diff + [highlight_diff(job_envs[i], 44)] + list(map(gen_env, extra_env)))
-        print('\033[1;44mRunning job %d%s: %s\033[1;m' %(i+1, ' (allow_failures)' if job_envs[i] in allow_failures else '', cmd_diff))
-
-        proc = subprocess.Popen(bash, stdin=subprocess.PIPE)
-        proc.communicate(cmd.encode())
-        if proc.returncode:
-            print('\033[1;41mFailed job %d: %s\033[1;m' %(i+1, cmd))
-            if job_envs[i] not in allow_failures:
-                sys.exit(proc.returncode)
-
-if __name__ == "__main__":
-    main(os.path.abspath(os.path.dirname(__file__)), sys.argv)
