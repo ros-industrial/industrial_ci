98abcaeca307ee99f8761a34f2b0ed085da19114
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2021-11-18 15:25:29 -0500

    Attempt #7 https://gitlab.com/gitlab-examples/ssh-private-key/-/issues/1#note_48668712

--- gitlab.sh
+++ gitlab.sh
@@ -36,7 +36,7 @@ if [ -n "$SSH_PRIVATE_KEY" ]; then
   eval $(ssh-agent -s)
   # Avoiding https://github.com/ros-industrial/industrial_ci/issues/756
   # add key to agent
-  echo -e "${SSH_PRIVATE_KEY}" | ssh-add - > /dev/null || { res=$?; echo "could not add ssh key"; exit $res; }
+  echo "${SSH_PRIVATE_KEY}" | ssh-add - > /dev/null || { res=$?; echo "could not add ssh key"; exit $res; }
   if [ -n "$SSH_SERVER_HOSTKEYS" ]; then
     mkdir -p ~/.ssh
     # setup known hosts
