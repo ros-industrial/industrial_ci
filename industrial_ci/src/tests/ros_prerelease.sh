318aa389292efa62f8d9941392d756472ca6e965
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-03-31 17:06:21 +0200

    Use UNDERLAY, but set it to /opt/ros/noetic for ROS1
    This reverts commit f8662540452513c018a09ba7f29fbaf9b1030396.

--- industrial_ci/src/tests/ros_prerelease.sh
+++ industrial_ci/src/tests/ros_prerelease.sh
@@ -40,7 +40,9 @@ function setup_ros_prerelease() {
     ici_setup_git_client
 
     if [ "$BUILDER" != "colcon" ]; then
+        # We always use a Noetic docker image for ROS1
         ici_install_pkgs_for_command catkin_test_results ros-noetic-catkin
+        UNDERLAY=/opt/ros/noetic
     else
         ici_source_builder
         builder_setup
@@ -118,7 +120,11 @@ function run_ros_prerelease() {
     ici_step "prepare_prerelease_workspaces" ici_cmd prepare_prerelease_workspaces "$WORKSPACE" "$reponame" "$(basename "$TARGET_REPO_PATH")"
     ici_step 'generate_prerelease_script' ici_cmd sudo -EH -u ci generate_prerelease_script.py "${ROSDISTRO_INDEX_URL}" "$PRERELEASE_DISTRO" default "$OS_NAME" "$OS_CODE_NAME" "${OS_ARCH:-amd64}" --build-tool "$BUILDER" --level "$downstream_depth" --output-dir "$WORKSPACE" --custom-repo "$reponame::::"
 
-    ABORT_ON_TEST_FAILURE=1 ici_step "run_prerelease_script" ici_cmd sudo -EH -u ci sh -c ". /opt/ros/noetic/setup.sh && cd '$WORKSPACE' && exec ./prerelease.sh -y"
+    local setup_sh=
+    if [ -f "${UNDERLAY:?}/setup.sh" ]; then
+        setup_sh=". $UNDERLAY/setup.sh && "
+    fi
+    ABORT_ON_TEST_FAILURE=1 ici_step "run_prerelease_script" ici_cmd sudo -EH -u ci sh -c "${setup_sh}cd '$WORKSPACE' && exec ./prerelease.sh -y"
 
     ici_log 'ROS Prerelease Test went successful.'
 }
